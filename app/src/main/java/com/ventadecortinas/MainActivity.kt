e22f1f781cab5c65a22ed95f8f4e30b29b9678b2
Harryet19 <mileidy7garcia@gmail.com>
2021-11-21 15:14:06 -0500

    Corrección código que aparecía rojo de las pantallas de 5" y 10", y del logo.

--- app/src/main/java/com/ventadecortinas/MainActivity.kt
+++ app/src/main/java/com/ventadecortinas/MainActivity.kt
@@ -8,11 +8,7 @@ public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_main)
 
-    public void irLista (View view){
-        Intent i= new Intent(packageContext this, ConsultaVentasActivity.class);
-        startActivity(i);
     }
 }
