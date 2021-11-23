c93f54fad419b4040149f110ae4f7cbea7b425a0
Luisemar <luisemar@gmail.com>
2021-11-23 10:55:25 -0500

    commit app bar

--- app/src/main/java/com/ventadecortinas/MainActivity.kt
+++ app/src/main/java/com/ventadecortinas/MainActivity.kt
@@ -3,11 +3,16 @@ package com.ventadecortinas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.main_toolbar))
     }
 
     fun onRegVendedor(botonRegVend: android.view.View) {
@@ -18,5 +23,28 @@ class MainActivity : AppCompatActivity() {
     fun onIngVendedor(view: android.view.View) {
                 val intento = Intent(this, IniciarSesionActivity::class.java)
                 startActivity(intento)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId){
+        R.id.action_search ->{
+           Toast.makeText(this,R.string.txt_action_search,Toast.LENGTH_LONG).show()
+             true
+        }
+        R.id.action_setting ->{
+            Toast.makeText(this,R.string.txt_action_settings,Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_Logout ->{
+            Toast.makeText(this,R.string.txt_action_logout,Toast.LENGTH_LONG).show()
+            true
+        }
+        else ->{
+            super.onOptionsItemSelected(item)
+        }
     }
 }
