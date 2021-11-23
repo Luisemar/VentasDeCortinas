3ea69e6380b6cafe52f5616a2b88ffb4a9a2c6be
Luisemar <luisemar@gmail.com>
2021-11-22 21:07:29 -0500

    commit validar ingreso vendedor

--- app/src/main/java/com/ventadecortinas/IniciarSesionActivity.kt
+++ app/src/main/java/com/ventadecortinas/IniciarSesionActivity.kt
@@ -1,11 +1,29 @@
 package com.ventadecortinas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class IniciarSesionActivity : AppCompatActivity() {
+
+    private var edtUsername: EditText? = null
+    private var edtPassword: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        edtUsername = findViewById(R.id.edtUsername)
+        edtPassword = findViewById(R.id.edtPassword)
+    }
+
+    fun onIniSesion(view: android.view.View) {
+        if (edtUsername!!.text.toString() == "e6@email.com") {
+            if (edtPassword!!.text.toString() == "1") {
+                val intento = Intent(this, OpcionesVendedorActivity::class.java)
+                startActivity(intento)
+            }
+        }
     }
 }
--- app/src/main/java/com/ventadecortinas/MainActivity.kt
+++ app/src/main/java/com/ventadecortinas/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onIngVendedor(view: android.view.View) {
-        val intento = Intent(this, OpcionesVendedorActivity::class.java)
-        startActivity(intento)
+                val intento = Intent(this, IniciarSesionActivity::class.java)
+                startActivity(intento)
     }
 }
